bbcd68623d453483997479d92cd6fc928a68da95
GreenAlex7 <57642040+GreenAlex7@users.noreply.github.com>
2020-02-29 09:55:36 +0300

    Update KuAV.c

--- KuAV.c
+++ KuAV.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define Q 5
-#define Z 5
-
+#define Q 15
+#define Z 15
 
 int main()
 {
-	srand(time(0));
+    srand(time(0));
 	int A[Q][Z];
 	int i, j;
 
-	for (i = 0; i < 5; i++) {
-		for (j = 0; j < 5; j++) {
+	for (i = 0; i < Q; i++) {
+		for (j = 0; j < Z; j++) {
 			if (i == 0 || j == 0) {
 				A[i][j] = 1;
 			}
 			else {
 				A[i][j] = rand()%2;
 			}
-			if (i % 4 == 0) {
+			if (i % (Q - 1) == 0) {
 				A[i][j] = 1;
 			}
-			else if (j % 4 == 0) {
+			else if (j % (Z - 1) == 0) {
 				A[i][j] = 1;
 			}
 
@@ -31,12 +30,12 @@ int main()
 
 	printf("Square:\n");
 	int number = 0;
-	for (i = 0; i < 5; i++) {
-		for (j = 0; j < 5; j++)
+	for (i = 0; i < Q; i++) {
+		for (j = 0; j < Z; j++)
 		{
 			printf("%d", A[i][j]);
 			number++;
-			if (number == 5)
+			if (number == 15)
 			{
 				printf("\n");
 				number = 0;
